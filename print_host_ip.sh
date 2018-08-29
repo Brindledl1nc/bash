fa26be2eaa4be1cc73b8dbd6843d6656fe0c1fae
Your Name <evangelinekoski@hotmail.com>
2018-08-29 15:42:48 -0500

    Errors list

--- print_host_ip.sh
+++ print_host_ip.sh
@@ -4,6 +4,6 @@ hostname
 /sbin/ifconfig eth0
 uname-a
 IP address 172-31-89-134
-CPU core 1
-Processor 0
-[ec2-user@ip-172-31-89-134 ~]
+echo "cat proc/cpuinfo" >> file_name test123
+echo "Free Mem = $(free -m)\n" >> file_name test123
+echo "$(ifconfig etho)" >> file_name test123
