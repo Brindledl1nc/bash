bcf115876460e6dfe2342c57f6b860e6ace349cd
Your Name <evangelinekoski@hotmail.com>
2018-08-30 13:05:49 -0500

    Adjusted the character

--- conditions/num.sh
+++ conditions/num.sh
@@ -6,7 +6,8 @@ if [ "$number" -gt 1 ] && [ "$number" -lt 10 ]; then
 else
 	echo "Wrong insertion !"
 fi
+
 # Create another script called num1.sh,
 # Take 2 values if either of numbers of less than 10
-# add them and print the result else, multuply them and print
+# add them and print the result else, multiply them and print
 # the result. 
